b9872f58b2ac34818c0a9e194763762a5bedcb5d
Iizensx <thanaphat.kre@spumail.net>
2026-03-22 19:25:03 +0700

    Dupm ล่าสุด

--- obj/Debug/net10.0/66022380.AssemblyInfo.cs
+++ obj/Debug/net10.0/66022380.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("66022380")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1bb78b744e5aa0c72d907518353b54bbf3f0ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+903ad7eecdd1f47f4e351aeab229b56c5c5a6021")]
 [assembly: System.Reflection.AssemblyProductAttribute("66022380")]
 [assembly: System.Reflection.AssemblyTitleAttribute("66022380")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
